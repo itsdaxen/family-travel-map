6a1b3313c5cc881a21615cae84b0340429862cd6
itsdaxen <fa.rzam.sadegh@gmail.com>
2025-05-14 06:30:19 +0300

    Add body-parser middleware and implement country addition feature

--- visited_countries/index.js
+++ visited_countries/index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+// Add body-parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
@@ -10,10 +14,20 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
+let countries = ["fi", "se", "de", "fr", "it", "es", "pt", "pl", "cz"];
+let total = countries.length;
+countries = countries.map((country) => country.toUpperCase());
+
+function addCountry(country) {
+  countries.push(country.toUpperCase());
+  total = countries.length;
+}
+
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.render("index.ejs", { countries, total });
 });
 
-app.get("/countries", (req, res) => {
-  res.send("Hello World");
+app.post("/add", (req, res) => {
+  addCountry(req.body.country.toUpperCase());
+  res.redirect("/");
 });
